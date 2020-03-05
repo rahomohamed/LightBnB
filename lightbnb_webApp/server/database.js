f559130e5e54d50704058e22389555c536b1a795
rahomohamed <rahojmohamed@gmail.com>
2020-03-05 19:40:37 +0000

    added getallmyreservations query

--- lightbnb_webApp/server/database.js
+++ lightbnb_webApp/server/database.js
@@ -17,16 +17,8 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query(`SELECT * FROM users
+  WHERE email = $1`, [email]).then(res => res.rows[0])
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -36,7 +28,8 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`SELECT * FROM users
+    WHERE id = $1`, [id]).then(res => res.rows[0])
 }
 exports.getUserWithId = getUserWithId;
 
@@ -47,10 +40,8 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+return pool.query(`INSERT INTO users (name, email, password)
+VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password]).then(res => res.rows[0])
 }
 exports.addUser = addUser;
 
@@ -62,7 +53,15 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+return pool.query(`SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
+WHERE reservations.guest_id = $1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT $2`, [guest_id, limit]).then(res => res.rows)
 }
 exports.getAllReservations = getAllReservations;
 
