ecd1b65399afc035964a992c9b255fffe8bfe1bc
rahomohamed <rahojmohamed@gmail.com>
2020-03-05 22:12:39 +0000

    addProperty.js

--- lightbnb_webApp/server/database.js
+++ lightbnb_webApp/server/database.js
@@ -81,7 +81,7 @@ const getAllProperties = function(options, limit = 10) {
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
-  WHERE true`;
+  WHERE true `;
 
   // 3
   if (options.city) {
@@ -105,7 +105,7 @@ const getAllProperties = function(options, limit = 10) {
       queryParams.push(`%${options.minimum_rating}%`);
       queryString += `AND property_reviews.rating >= $${queryParams.length} `;
       }
-      
+
   // 4
   queryParams.push(limit);
   queryString += `
@@ -130,9 +130,13 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+  return pool.query(
+      `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country,  parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1,$2,$3,$4, $5, $6,$7,$8, $9, $10, $11, $12 , $13, $14)
+  RETURNING *;
+  `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url,  property.cost_per_night, property.street, property.city, property.province,property.post_code,  property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]
+  )
+    .then(res => res.rows[0]);
+  }
 exports.addProperty = addProperty;
